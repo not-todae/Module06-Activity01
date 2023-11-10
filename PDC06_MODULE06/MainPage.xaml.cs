1905cd952711e6f9684bdd62807d0c4134e613a8
not-todae <echon.davidaaron@auf.edu.ph>
2023-11-10 11:48:06 +0800

    first commit

---
+++ PDC06_MODULE06/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PDC06_MODULE06
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Accelerometer_Page_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new AccelerometerPage());
+        }
+        private async void Connectivity_Page_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ConnectivityPage());
+        }
+        private async void DeviceBattery_Page_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new DeviceBatteryPage());
+        }
+        private async void Geolocation_Page_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new GeolocationPage());
+        }
+    }
+}
